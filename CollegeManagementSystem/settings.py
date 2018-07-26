e345334e1805a4e8d390eed2f5feec8dbd4445e7
Mathew <mathewthomas358@gmail.com>
2018-07-26 23:49:51 +0530

    Added new module

--- CollegeManagementSystem/settings.py
+++ CollegeManagementSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'staffadvisor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'CollegeManagementSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "assets"),
+)
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ staffadvisor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StaffadvisorConfig(AppConfig):
+    name = 'staffadvisor'
